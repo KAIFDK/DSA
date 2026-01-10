ab1584245bf2dfe645520549c87bcf9cd122a876
KAIFDK <156346760+KAIFDK@users.noreply.github.com>
2026-01-10 09:46:39 +0000

    learned Singly Linked List

---
+++ SinglelyLL.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self,info,next = None):
+        self.data = info
+        self.next = next
+
+class SinglyLL:
+    def __init__(self,head = None):
+        self.head = head
+
+    def insertAtbegining(self,value):
+        temp = Node(value)
+        temp.next = self.head
+        self.head = temp
+
+    def insertAtMid(self,value,x):
+        temp = Node(value)
+        t1 = self.head
+        while(t1.next != None):
+            if(t1.data == x):
+                temp.next = t1.next
+                t1.next = temp
+            t1 = t1.next
+
+    def insertAtEnd(self,value):
+        temp = Node(value)
+        if(self.head != None):
+            t1 = self.head
+            while(t1.next != None):
+                t1 = t1.next
+            t1.next = temp
+        else:
+            self.head = temp
+
+    def delete(self,value):
+        t1 = self.head
+        prv = t1
+        if(t1.data == value):
+            self.head = t1.next
+        while(t1.next != None):
+            if(t1.data == value):
+                prv.next = t1.next
+                break
+            else:
+                prv = t1
+                t1 = t1.next
+        if(t1.data == value):
+            prv.next = None
+
+    def printLL(self):
+        t1 = self.head
+        while (t1.next != None):
+            print(t1.data)
+            t1 = t1.next
+        print(t1.data)
+
+obj = SinglyLL()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtMid(50,20)
+obj.insertAtbegining(5)
+obj.delete(30)
+obj.printLL()
