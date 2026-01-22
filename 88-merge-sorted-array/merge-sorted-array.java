971dbae394e03976f3b73dda865e06e70b10aa55
KAIF <156346760+KAIFDK@users.noreply.github.com>
2026-01-22 23:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (19.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,20 +1,20 @@
 class Solution {
     //KAIFDK
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int mindx = m - 1;
-        int nindx = n - 1;
-        int right = m + n - 1;
+      int midx = m - 1;
+      int nidx = n - 1 ;
+      int right = m + n -1;
 
-        while(nindx >= 0){
-            if(mindx >= 0 && nums1[mindx] > nums2[nindx]){
-                nums1[right] = nums1[mindx];
-                mindx--;
-            }
-            else{
-                nums1[right] = nums2[nindx];
-                nindx--; 
-            }
-            right --;
-        }    
+      while(nidx >= 0){
+        if(midx >= 0 && nums1[midx] > nums2[nidx]){
+            nums1[right] = nums1[midx];
+            midx--;
+        }
+        else{
+            nums1[right] = nums2[nidx];
+            nidx--;
+        }
+        right--;
+      }    
     }
 }
