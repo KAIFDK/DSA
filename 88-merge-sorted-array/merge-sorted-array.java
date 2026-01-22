d3660de1b0d5038fc7ca861071a6edf4f78065fa
KAIF <156346760+KAIFDK@users.noreply.github.com>
2026-01-22 22:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (19.33%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int mindx = m - 1;
+        int nindx = n - 1;
+        int right = m + n - 1;
+
+        while(nindx >= 0){
+            if(mindx >= 0 && nums1[mindx] > nums2[nindx]){
+                nums1[right] = nums1[mindx];
+                mindx--;
+            }
+            else{
+                nums1[right] = nums2[nindx];
+                nindx--; 
+            }
+            right --;
+        }    
+    }
+}
