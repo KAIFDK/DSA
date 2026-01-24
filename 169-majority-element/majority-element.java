e8c28623c0ac91be3ba14df2387a1ec683c7a7f1
KAIF <156346760+KAIFDK@users.noreply.github.com>
2026-01-24 16:15:31 +0530

    Time: 19 ms (12.16%) | Memory: 52.5 MB (92.62%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        //KAIFDK
+        HashMap<Integer,Integer> hash= new HashMap();
+
+        int maj = 0;
+        int res = 0;
+
+        for(int n : nums){
+            hash.put(n,1 + hash.getOrDefault(n,0));
+            if(hash.get(n) > maj){
+                res = n;
+                maj = hash.get(n);
+            }
+        }
+        return res;
+    }
+}
