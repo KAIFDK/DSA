dc2750418dd15eb496ad7583fa46e4b7596549c6
KAIF <156346760+KAIFDK@users.noreply.github.com>
2026-01-26 21:50:21 +0530

    Time: 0 ms (100.00%) | Memory: 63.3 MB (16.57%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,17 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         //KAIFDK
-        int n = nums.length;
-        k = k % n;
-        int [] rotated = new int[n];
+        k = k % nums.length;
+        if(k != 0){
+            int[] temp = new int[nums.length];
+            System.arraycopy(nums,nums.length - k,temp,0,k);
+            System.arraycopy(nums,0,temp,k,nums.length-k);
+            System.arraycopy(temp,0,nums,0,nums.length);
 
-        for(int i = 0 ; i < n ; i++){
-            rotated[(i + k) % n] = nums[i];
-        }
 
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = rotated[i];
         }
-        
     }
 }
